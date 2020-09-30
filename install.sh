150ae656fcced76f65c443bd63ed1352d615f628
NAndz <60912687+Nandz024@users.noreply.github.com>
2020-09-30 18:27:17 +0530

    Updated install.sh
    removed some error

--- install.sh
+++ install.sh
@@ -54,8 +54,6 @@ GO111MODULE=on go get -u -v github.com/projectdiscovery/dnsprobe
 
 GO111MODULE=auto go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
 
-GO111MODULE=auto go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
-
 GO111MODULE=on go get -u github.com/projectdiscovery/chaos-client/cmd/chaos
 
 go get github.com/ffuf/ffuf
